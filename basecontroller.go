0dbb8971d1a516fbac86d6f677842e1355a7311e
TedForV <TedForV@outlook.com>
2018-04-16 15:06:07 +0800

    register api route into gin router

--- basecontroller.go
+++ basecontroller.go
@@ -5,9 +5,14 @@ import (
 	"reflect"
 	"strings"
 	"errors"
+	"net/http"
 )
 
-type BaseController interface {
+const (
+	KEY_SEPERATOR = ":"
+)
+
+type IBaseController interface {
 	Get(c *gin.Context)
 	Post(c *gin.Context)
 	Put(c *gin.Context)
@@ -16,12 +21,81 @@ type BaseController interface {
 	Head(c *gin.Context)
 	Options(c *gin.Context)
 
-	Mapping(ginEngine *gin.Engine)
+	//
+	Mapping()(map[string]GinHandler)
 }
+
+type BaseController struct{
+
+}
+
+func (t *BaseController) Get(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Post(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Put(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Delete(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Patch(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Head(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+func (t *BaseController) Options(c *gin.Context) {
+	returnNotResource(c)
+	return
+}
+
+func (t *BaseController) Mapping()(map[string]GinHandler) {
+	return nil
+}
+
+func returnNotResource(c *gin.Context){
+	c.String(http.StatusNotFound,"")
+}
+
+func analyseMappingKey(key string)(method string,pathName string,err error){
+	key = strings.TrimSpace(key)
+
+	if len(key) < 5{
+		return "","",errors.New("key has error.")
+	}
+
+	if i := strings.Index(key,KEY_SEPERATOR); i == -1{
+		return  "","",errors.New("key needs a comma.")
+	} else{
+		method = key[:i]
+		if err != nil{
+			return "","",errors.New("key has error.")
+		}
+
+		pathName = key[i+1:]
+	}
+
+	return method,pathName,nil
+}
+
+//method is http.MethodXXX
+func ComposeCustomMappingKey(method string,path string) string{
+	return method + KEY_SEPERATOR + path
+}
+
 
 type GinHandler func(c *gin.Context)
 
-func RegisterAPIRoute(ginEngine *gin.Engine, controllers []BaseController) {
+func RegisterAPIRoute(ginEngine *gin.Engine, controllers []IBaseController) {
 	if controllers == nil || len(controllers) == 0 {
 		return
 	}
@@ -30,7 +104,11 @@ func RegisterAPIRoute(ginEngine *gin.Engine, controllers []BaseController) {
 		if err != nil {
 			panic(err)
 		}
-		autoMapping(ginEngine, cname, &c)
+		autoMapping(ginEngine, cname, c)
+		err = autoCustomMapping(ginEngine,cname,c)
+		if err != nil{
+			panic(err)
+		}
 	}
 
 }
@@ -40,7 +118,7 @@ const (
 	ERROR_CONTROLLER_NAME = "Controller name must be suffix with 'Controller'"
 )
 
-func getControllerValidName(controller BaseController) (string, error) {
+func getControllerValidName(controller IBaseController) (string, error) {
 	typeInfo := reflect.TypeOf(controller)
 	fullName := typeInfo.Elem().String()
 	lastDotIndex := strings.LastIndex(fullName, ".")
@@ -54,5 +132,84 @@ func getControllerValidName(controller BaseController) (string, error) {
 
 }
 
-func autoMapping(ginEngine *gin.Engine, controllerName string, controller BaseController) {
+func autoMapping(ginEngine *gin.Engine, controllerName string, controller IBaseController) {
+	path := "/" + controllerName
+	ginEngine.GET(path, func(c *gin.Context) {
+		controller.Get(c)
+	})
+	ginEngine.POST(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+	ginEngine.PUT(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+	ginEngine.DELETE(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+	ginEngine.HEAD(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+	ginEngine.OPTIONS(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+	ginEngine.PATCH(path, func(c *gin.Context) {
+		controller.Post(c)
+	})
+}
+
+func autoCustomMapping(ginEngine *gin.Engine,controllerName string,controller IBaseController) error{
+	 route := controller.Mapping()
+
+	 for k,v :=range route{
+	 	 method,path,err:= analyseMappingKey(k)
+	 	 if err != nil{
+	 	 	return  err
+		 }
+		 fullPath := "/"+controllerName + "/" + path
+		 switch method {
+		 case http.MethodGet:
+		 	func(handler GinHandler){
+				ginEngine.GET(fullPath, func(c *gin.Context) {
+					handler(c)
+				})
+			}(v)
+		 case http.MethodPost:
+		 	func(handler GinHandler){
+		 		ginEngine.POST(fullPath, func(c *gin.Context) {
+					handler(c)
+				})
+			}(v)
+		 case http.MethodPut:
+			 func(handler GinHandler){
+				 ginEngine.PUT(fullPath, func(c *gin.Context) {
+					 handler(c)
+				 })
+			 }(v)
+		 case http.MethodDelete:
+			 func(handler GinHandler){
+				 ginEngine.DELETE(fullPath, func(c *gin.Context) {
+					 handler(c)
+				 })
+			 }(v)
+		 case http.MethodHead:
+			 func(handler GinHandler){
+				 ginEngine.HEAD(fullPath, func(c *gin.Context) {
+					 handler(c)
+				 })
+			 }(v)
+		 case http.MethodOptions:
+			 func(handler GinHandler){
+				 ginEngine.OPTIONS(fullPath, func(c *gin.Context) {
+					 handler(c)
+				 })
+			 }(v)
+		 case http.MethodPatch:
+			 func(handler GinHandler){
+				 ginEngine.PATCH(fullPath, func(c *gin.Context) {
+					 handler(c)
+				 })
+			 }(v)
+		 }
+	 }
+	 return  nil
 }
--- basecontroller_test.go
+++ basecontroller_test.go
@@ -2,8 +2,9 @@ package RestfulRouter
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/gin-gonic/gin"
 	"testing"
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func TestGetControllerValidName(t *testing.T) {
@@ -15,31 +16,44 @@ func TestGetControllerValidName(t *testing.T) {
 	assert.Equal(t, "Test", name)
 }
 
+func TestAnalyseMappingKey1(t *testing.T){
+	temp1 := ComposeCustomMappingKey(http.MethodGet,"test/:userName")
+	assert.Equal(t,"GET:test/:userName",temp1)
+	method,path,err := analyseMappingKey(temp1)
+	if err != nil{
+		t.Error(err)
+	}
+	assert.Equal(t,http.MethodGet,method)
+	assert.Equal(t,"test/:userName",path)
+}
+
+func TestAnalyseMappingKey2(t *testing.T){
+	temp1 := ComposeCustomMappingKey(http.MethodGet,"test?_:userName")
+	assert.Equal(t,"GET:test?_:userName",temp1)
+	method,path,err := analyseMappingKey(temp1)
+	if err != nil{
+		t.Error(err)
+	}
+	assert.Equal(t,http.MethodGet,method)
+	assert.Equal(t,"test?_:userName",path)
+}
+
+
+
 type TestController struct {
+	BaseController
 }
 
-func (t *TestController) Get(c *gin.Context) {
-
-}
-func (t *TestController) Post(c *gin.Context) {
-
-}
-func (t *TestController) Put(c *gin.Context) {
-
-}
-func (t *TestController) Delete(c *gin.Context) {
-
-}
-func (t *TestController) Patch(c *gin.Context) {
-
-}
-func (t *TestController) Head(c *gin.Context) {
-
-}
-func (t *TestController) Options(c *gin.Context) {
-
+func (tc *TestController) Mapping()(map[string]GinHandler){
+	m := make(map[string]GinHandler,1)
+	m[ComposeCustomMappingKey(http.MethodGet,"customTest")] = CustomMethodTest
+	return m
 }
 
-func (t *TestController) Mapping(ginEngine *gin.Engine) {
+func CustomMethodTest(c *gin.Context){
+	c.String(http.StatusNotFound,"")
+}
 
+func TestRegisterAPIRoute(t *testing.T) {
+	RegisterAPIRoute(gin.Default(),[]IBaseController{&TestController{}})
 }
