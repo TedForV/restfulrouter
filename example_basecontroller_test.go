dd4888eb982fe771be436ad85bc508ec70f5574a
TedForV <TedForV@outlook.com>
2019-10-21 20:08:32 +0800

    remove test

--- example_basecontroller_test.go
+++ example_basecontroller_test.go
@@ -1,31 +1,30 @@
-package restfulrouter_test
+package restfulrouter
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"restfulrouter"
-)
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	"net/http"
+// )
 
-type TestController struct {
-	restfulrouter.BaseController
-}
+// type TestController struct {
+// 	restfulrouter.BaseController
+// }
 
-func (tc *TestController) Mapping() map[string]restfulrouter.GinHandler {
-	m := make(map[string]restfulrouter.GinHandler, 1)
-	m[restfulrouter.ComposeCustomMappingKey(http.MethodGet, "customTest")] = CustomMethodTest
-	return m
-}
+// func (tc *TestController) Mapping() map[string]restfulrouter.GinHandler {
+// 	m := make(map[string]restfulrouter.GinHandler, 1)
+// 	m[restfulrouter.ComposeCustomMappingKey(http.MethodGet, "customTest")] = CustomMethodTest
+// 	return m
+// }
 
-func CustomMethodTest(c *gin.Context) {
-	c.String(http.StatusNotFound, "")
-}
+// func CustomMethodTest(c *gin.Context) {
+// 	c.String(http.StatusNotFound, "")
+// }
 
-func ExampleRegisterAPIRoute() {
-	r := gin.Default()
-	restfulrouter.RegisterAPIRoute(r, []restfulrouter.IBaseController{&TestController{}})
-}
+// func ExampleRegisterAPIRoute() {
+// 	r := gin.Default()
+// 	restfulrouter.RegisterAPIRoute(r, []IBaseController{&TestController{}})
+// }
 
-func ExampleRegisterGroupAPIRoute() {
-	r := gin.Default()
-	restfulrouter.RegisterGroupAPIRoute("/api", r, []restfulrouter.IBaseController{&TestController{}})
-}
+// func ExampleRegisterGroupAPIRoute() {
+// 	r := gin.Default()
+// 	restfulrouter.RegisterGroupAPIRoute("/api", r, []restfulrouter.IBaseController{&TestController{}})
+// }
